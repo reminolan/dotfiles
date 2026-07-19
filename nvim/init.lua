6ed785b6290dfed241fdb80658bd3302e124a19f
frog_snax <remi@duphus.com>
2026-07-19 07:06:49 -0400

    switch to 2-space tabs; various little tweaks

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,93 +3,98 @@ vim.g.mapleader = ' '
 
 -- Bootstrap lazy.nvim
 local function BootstrapLazy(colorscheme, lazy_spec)
-   if type(colorscheme) ~= "string" or colorscheme == "" then
-      colorscheme = "default"
-   end
+  if type(colorscheme) ~= "string" or colorscheme == "" then
+    colorscheme = "default"
+  end
 
-   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-   if not (vim.uv or vim.loop).fs_stat(lazypath) then
-     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-     local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-     if vim.v.shell_error ~= 0 then
-       vim.api.nvim_echo({
-         { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-         { out, "WarningMsg" },
-         { "\nPress any key to exit..." },
-       }, true, {})
-       vim.fn.getchar()
-       os.exit(1)
-     end
-   end
-   vim.opt.rtp:prepend(lazypath)
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({
+      "git", "clone", "--filter=blob:none", "--branch=stable",
+      lazyrepo, lazypath
+    })
+    if vim.v.shell_error ~= 0 then
+      vim.api.nvim_echo({
+        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+        { out, "WarningMsg" },
+        { "\nPress any key to exit..." },
+      }, true, {})
+      vim.fn.getchar()
+      os.exit(1)
+    end
+  end
+  vim.opt.rtp:prepend(lazypath)
 
-   -- Make sure to setup `mapleader` and `maplocalleader` before
-   -- loading lazy.nvim so that mappings are correct.
-   -- This is also a good place to setup other settings (vim.opt)
-   vim.g.mapleader = " "
-   vim.g.maplocalleader = "\\"
+  -- Make sure to setup `mapleader` and `maplocalleader` before
+  -- loading lazy.nvim so that mappings are correct.
+  -- This is also a good place to setup other settings (vim.opt)
+  vim.g.mapleader = " "
+  vim.g.maplocalleader = "\\"
 
-   -- Setup lazy.nvim
-   lazy = require("lazy")
-   lazy.setup({
-     spec = lazy_spec,
-     -- Configure any other settings here. See the documentation for more details.
-     -- colorscheme that will be used when installing plugins.
-     install = { colorscheme = { colorscheme } },
-     -- automatically check for plugin updates
-     checker = { enabled = true },
-   })
+  -- Setup lazy.nvim
+  lazy = require("lazy")
+  lazy.setup({
+    spec = lazy_spec,
+    install = { colorscheme = { colorscheme } },
+    checker = { enabled = true },
+  })
 
-   return lazy
+  return lazy
 end
 
-local lazy = BootstrapLazy("everforest", {
-   {
-      "nvim-treesitter/nvim-treesitter",
-      branch = 'master',
-      lazy = false,
-      build = ":TSUpdate"
-   },
-   {
-      "neanias/everforest-nvim"
-   }
+local lazy = BootstrapLazy("adwaita", {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    lazy = false,
+    build = ":TSUpdate"
+  },
+  {
+    "neanias/everforest-nvim"
+  },
+  {
+    "Mofiqul/adwaita.nvim",
+    lazy = false,
+    priority = 1000,
+  }
 })
 
 require('nvim-treesitter.configs').setup({
-   ensure_installed = { 'c', 'cmake', 'lua', 'markdown', 'python' },
-   auto_install = true,
+  ensure_installed = { 'c', 'cmake', 'lua', 'markdown', 'python' },
+  auto_install = true,
 
-   highlight = {
-      enable = true
-   }
+  highlight = {
+    enable = true
+  }
 })
 
 vim.opt.background = 'dark'
 vim.opt.number = true 
 vim.opt.relativenumber = false
 vim.opt.spell = false
-vim.opt.wrap = false
 
-vim.opt.shiftwidth = 3
-vim.opt.tabstop = 3
-vim.opt.softtabstop = 3
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.cursorline = true
+vim.opt.colorcolumn="81"
 
 vim.opt.guifont = "Cascadia Mono NF:h13"
+vim.wo.wrap = false
 
-vim.cmd.colorscheme("everforest")
+vim.cmd.colorscheme("adwaita")
 
 vim.keymap.set('n', 'q', function()
-   vim.cmd([[:noh]])
+  vim.cmd([[:noh]])
 end)
 
 vim.keymap.set('n', '<C-l>', function()
-   vim.cmd([[:set cursorline!]])
+  vim.cmd([[:set cursorline!]])
 end)
 
 vim.keymap.set('n', '<C-c>', function()
-   vim.cmd([[:set cursorcolumn!]])
+  vim.cmd([[:set cursorcolumn!]])
 end)
-
 
